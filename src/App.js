fbe93957fa755eaf1917ac2c8b9047552128ec4c
pagatelabirra <raulmatiascabral@gmail.com>
2022-05-30 23:03:54 -0300

    No changes added on this commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar/NavBar'
 import './App.css';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
   return (
